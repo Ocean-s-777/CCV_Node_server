fb66754af967638ccb9a3dce514c0b34f690ee12
OxCone1 <t1lava01@students.oamk.fi>
2022-11-14 10:11:08 +0200

    Updated some server stuff

--- server.js
+++ server.js
@@ -1,16 +1,41 @@
+
+
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 };
 
 const express = require('express');
+const mongodb = require('mongodb');
+const port = process.env.PORT || 3000;
 
 const app = express();
+const MongoClient = mongodb.MongoClient;
 
-app.listen(process.env.PORT, () => {
-    console.log(`Running on port ${process.env.PORT}`)
+let cachedClient = null;
+let cachedDb = null;
+
+async function connectToDatabase() {
+    if (cachedDb) {
+        return cachedDb;
+    }
+    const client = await MongoClient.connect(process.env.DATABASE_URL, { 
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    const db = client.db('climate-change');
+
+    cachedClient = client;
+    cachedDb = db;
+    return db;
+};
+
+app.get('/db', async (req, res) => {
+    const db = await connectToDatabase();
+    const collection = await db.collection('db');
+    const data = await collection.find({}).toArray();
+    res.json(data);
 });
 
-//respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
-    res.send('Hello world')
+app.listen(port, () => {
+    console.log(`Running on port ${process.env.PORT}`)
 });
