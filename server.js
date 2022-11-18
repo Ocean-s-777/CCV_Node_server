7b959dd9223158ad23f1e783cf509bf2db03b674
raosk <oskari.rantamaula@gmail.com>
2022-11-18 21:57:15 +0200

    erased some empty lines

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
-
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-};
-
-const express = require('express');
-const mongodb = require('mongodb');
+const express = require("express");
+const mongodb = require("mongodb");
 const port = process.env.PORT || 3000;
-
 const app = express();
 const MongoClient = mongodb.MongoClient;
 
@@ -15,27 +12,27 @@ let cachedClient = null;
 let cachedDb = null;
 
 async function connectToDatabase() {
-    if (cachedDb) {
-        return cachedDb;
-    }
-    const client = await MongoClient.connect(process.env.DATABASE_URL, { 
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
-    const db = client.db('climate-change');
+  if (cachedDb) {
+    return cachedDb;
+  }
+  const client = await MongoClient.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  const db = client.db("climate-change");
 
-    cachedClient = client;
-    cachedDb = db;
-    return db;
-};
+  cachedClient = client;
+  cachedDb = db;
+  return db;
+}
 
-app.get('/db', async (req, res) => {
-    const db = await connectToDatabase();
-    const collection = await db.collection('db');
-    const data = await collection.find({}).toArray();
-    res.json(data);
+app.get("/db", async (req, res) => {
+  const db = await connectToDatabase();
+  const collection = await db.collection("db");
+  const data = await collection.find({}).toArray();
+  res.json(data);
 });
 
 app.listen(port, () => {
-    console.log(`Running on port ${process.env.PORT}`)
+  console.log(`Running on port ${process.env.PORT}`);
 });
