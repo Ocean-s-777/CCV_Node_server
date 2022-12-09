6e2ac80f72f9772bed018337d1f605f3faa5edb0
OxCone1 <t1lava01@students.oamk.fi>
2022-12-09 02:26:45 +0200

    Added few database search functions

--- middleware/connectToDB.js
+++ middleware/connectToDB.js
@@ -5,18 +5,42 @@ let cachedClient = null;
 let cachedDb = null;
 
 async function connectToDatabase() {
-    if (cachedDb) {
-      return cachedDb;
-    }
-    const client = await MongoClient.connect(process.env.DATABASE_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    const db = client.db("climate-change");
-  
-    cachedClient = client;
-    cachedDb = db;
-    return db;
+  if (cachedDb) {
+    return cachedDb;
   }
+  const client = await MongoClient.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  const db = client.db("climate-change");
 
-module.exports = { connectToDatabase }
+  cachedClient = client;
+  cachedDb = db;
+  return db;
+}
+
+async function returnUserCollection() {
+  const db = await connectToDatabase();
+  const userCollection = await db.collection("users");
+  return userCollection;
+}
+
+async function returnGenCollection() {
+  const db = await connectToDatabase();
+  const generated = await db.collection("generatedViews");
+  return generated;
+}
+
+async function returnByUsername(username) {
+  const userCollection = await returnUserCollection();
+  const user = await userCollection.findOne({ username: username });
+  return user;
+}
+
+async function returnById(id) {
+  const userCollection = await returnUserCollection();
+  const user = await userCollection.findOne({ id: id });
+  return user;
+}
+
+module.exports = { connectToDatabase, returnUserCollection, returnGenCollection, returnByUsername };
